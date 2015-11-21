aba2cd5dd7ce220cea15e95a8666ec49fff66ae3
madprops <manuelchaves@gmail.com>
2015-11-20 18:46:15 -0600

    concil

--- chayotico/settings.py
+++ chayotico/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'chayotico',                      # Or path to database file if using sqlite3.
-        'USER': 'debaser',                      # Not used with sqlite3.
-        'PASSWORD': '420debaser',                  # Not used with sqlite3.
+        'USER': 'devaser',                      # Not used with sqlite3.
+        'PASSWORD': 'devaser',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
