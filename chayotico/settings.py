076db391d9a31a1842959343e1d8817eed446bed
madprops <manuelchaves@gmail.com>
2015-11-20 18:58:10 -0600

    rethad

--- chayotico/settings.py
+++ chayotico/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'saiodfj$@iabasosdjfo#@@#$ioasjdiefoisasf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['326chan.org',]
 
