85358a20eb82cb57a33d9802754dfe54ed95c4e6
madprops <manuelchaves@gmail.com>
2016-08-09 19:05:54 -0600

    '3fa'

--- chayotico/settings.py
+++ chayotico/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'chayotico.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'uygyu',                      # Or path to database file if using sqlite3.
-        'USER': '87yiuh',                      # Not used with sqlite3.
-        'PASSWORD': '98u8hj9',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
